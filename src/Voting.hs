7b6f0865e206cc1b54b4daa2068791faad5beabf
ClathomasPrime <thoma466@purdue.edu>
2018-12-12 23:06:03 -0500

    idk man

---
+++ src/Voting.hs
@@ -0,0 +1,14 @@
+
+module Voting where
+
+
+import Control.Monad.Random
+import Data.List
+import Data.Function
+import Data.Ratio
+
+import Util
+import LinearPref
+import GeneralD
+
+type Space = [Point Double]
